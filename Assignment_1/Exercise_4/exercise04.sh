09fe7e2eb062491f2260300281896e83054e7037
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2020-05-13 16:25:42 +0200

    Added ressult to nr.3

--- Assignment_1/Exercise_4/exercise04.sh
+++ Assignment_1/Exercise_4/exercise04.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 grep -Eciw "chin(a|ese)" *.tsv > aufg1.txt
 for i in `ls *.tsv`; do echo China in $i:`cut -f9 $i | grep -Eicw "chin(a|ese)"`; done > aufg2.txt; echo Summe:`cut *.tsv -f9 | grep -Eciw "chin(a|ese)"` > aufg2.txt
+cut -f6 *.tsv | grep -Ec "([[:digit:]]-[[:digit:]])" > loesungen/aufg3.txt
