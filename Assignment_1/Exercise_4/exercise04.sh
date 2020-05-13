180b3a0ae8d4e86038aca9dc9c5c91b0c403329f
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2020-05-13 16:28:38 +0200

    Added ressult to Exercise 4 nr.4

--- Assignment_1/Exercise_4/exercise04.sh
+++ Assignment_1/Exercise_4/exercise04.sh
@@ -2,3 +2,4 @@
 grep -Eciw "chin(a|ese)" *.tsv > aufg1.txt
 for i in `ls *.tsv`; do echo China in $i:`cut -f9 $i | grep -Eicw "chin(a|ese)"`; done > aufg2.txt; echo Summe:`cut *.tsv -f9 | grep -Eciw "chin(a|ese)"` > aufg2.txt
 cut -f6 *.tsv | grep -Ec "([[:digit:]]-[[:digit:]])" > aufg3.txt
+cut -f2 *.tsv | grep -viw "creator" | sed '/^$/d' > aufg4.txt
