1a6bb2b5c82d3d1f7a4fd5d6a1f39c37ea77b33a
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2020-05-18 13:04:52 +0200

    Fixed 2nd and 3rd command (now working properly)

--- Assignment_1/Exercise_4/exercise04.sh
+++ Assignment_1/Exercise_4/exercise04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 grep -Eciw "chin(a|ese)" *.tsv > aufg1.txt
-for i in `ls *.tsv`; do echo China in $i:`cut -f9 $i | grep -Eicw "chin(a|ese)"`; done > aufg2.txt; echo Summe:`cut *.tsv -f9 | grep -Eciw "chin(a|ese)"` > aufg2.txt
-cut -f6 *.tsv | grep -Ec "([[:digit:]]-[[:digit:]])" > aufg3.txt
+for i in `ls *.tsv`; do echo China/ese in $i:`cut -f9 $i | grep -Eicw "chin(a|ese)"`; done > aufg2.txt; echo Summe:`cut *.tsv -f9 | grep -Eciw "chin(a|ese)"` >> aufg2.txt
+cut -f6 *.tsv | grep -Eci "\b([[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]][[:digit:]]([[:digit:]]|x))" > aufg3.txt
 cut -f2 *.tsv | grep -viw "creator" | sed '/^$/d' > aufg4.txt
 cut -f2 *.tsv | grep -viw "creator" | sed '/^$/d' | sort | uniq > aufg5.txt
 cut -f6 *.tsv | grep -E "([[:digit:]]-[[:digit:]])" | sort > aufg6.txt; uniq aufg6.txt uniq-issn.txt
