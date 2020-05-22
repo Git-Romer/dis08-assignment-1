23c7f10511d8cc902166ce17f7491998d73db908
Gilles Romer <gilles_alain_luc.romer@smail.th-koeln.de>
2020-05-22 13:04:50 +0200

    Reworked the code to Nr.3, now being shorter

--- Assignment_1/Exercise_4/exercise04.sh
+++ Assignment_1/Exercise_4/exercise04.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 grep -Eciw "chin(a|ese)" *.tsv > aufg1.txt
 for i in `ls *.tsv`; do echo China/ese in $i:`cut -f9 $i | grep -Eicw "chin(a|ese)"`; done > aufg2.txt; echo Summe:`cut *.tsv -f9 | grep -Eciw "chin(a|ese)"` >> aufg2.txt
-cut -f6 *.tsv | grep -Eci "\b([[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]][[:digit:]]([[:digit:]]|x))" > aufg3.txt
+cut -f6 *.tsv | grep -Eci "\b(([[:digit:]]{4})-([[:digit:]]{3})([[:digit:]]|x))" > aufg3.txt
 cut -f2 *.tsv | grep -viw "creator" | sed '/^$/d' > aufg4.txt
 cut -f2 *.tsv | grep -viw "creator" | sed '/^$/d' | sort | uniq > aufg5.txt
 cut -f6 *.tsv | grep -Ewi "([[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]][[:digit:]]([[:digit:]]|x))" | sort > aufg6.txt; uniq aufg6.txt uniq-issn.txt
